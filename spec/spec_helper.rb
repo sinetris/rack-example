d952205f00e66a81ca894443eef617c4841c5404
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2014-08-11 04:29:36 +0200

    fix typo in spec_helper require support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[File.expand_path('../support/**/*.rb"', __FILE__)].each { |f| require f }
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
